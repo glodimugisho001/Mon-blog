adcc7f09afd875f9cf34c725c0d60bfa5359c576
Glodi Mugisho <glodiperso24@gmail.com>
2025-10-18 13:56:31 +0200

    remove useRouter not used

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import { Post } from "../app/types/blogType";
-import { useRouter } from "next/navigation";
 import { buttonVariants } from "./ui/button";
 import Image from "next/image";
 import {
